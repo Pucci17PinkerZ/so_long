ece5c6fd7fb091b6aa898dabed406fda5dbbd606
niko fio <nicofioradelfabro@gmail.com>
2025-12-13 13:20:11 +0100

    psy mon psy

--- src/player_move.c
+++ src/player_move.c
@@ -1,39 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_window.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pucci_pinker <pucci_pinker@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/26 15:57:15 by pucci             #+#    #+#             */
+/*   Updated: 2025/12/10 15:08:13 by pucci_pinke      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "so_long.h"
 
+int	close_window(t_game *game)
+{
+	free_map(game->map.map, game->map.column);
+	free_img_ptr(game);
+	mlx_destroy_window(game->mlx.mlx_ptr, game->mlx.win_ptr);
+	mlx_destroy_display(game->mlx.mlx_ptr);
+	free(game->mlx.mlx_ptr);
+	game->mlx.mlx_ptr = NULL;
+	init_game(game);
+	exit (0);
+	return (0);
+}
 
+void	move_player(t_game *game, int y, int x)
+{
+	if (game->map.map[game->player_y + y][game->player_x + x] == '1')
+	{
+		game->player_mov++;
+		return ;
+	}
+	else
+	{
+		if (game->map.map[game->player_y + y][game->player_x + x] == 'C')
+		{
+			game->map.map[game->player_y + y][game->player_x + x] = '0';
+			game->map.coins_found++;
+		}
+		game->map.map[game->player_y][game->player_x] = '0';
+		sprite_to_put(game, game->player_x, game->player_y);
+		game->map.map[game->player_y + y][game->player_x + x] = 'P';
+		game->player_y += y;
+		game->player_x += x;
+		sprite_to_put(game, game->player_x, game->player_y);
+		game->player_mov++;
+	}
+	ft_printf("position du joueur et de map[%d][%d]\n",
+		game->player_y, game->player_x);
+}
 
-// int	main(int argc, char **argv)
-// {
-// 	t_game	game;
+int	update_movement(int keycode, t_game *game)
+{
+	if (keycode == KEY_S)
+		move_player(game, 1, 0);
+	if (keycode == KEY_W)
+		move_player(game, -1, 0);
+	if (keycode == KEY_A)
+		move_player(game, 0, -1);
+	if (keycode == KEY_D)
+		move_player(game, 0, 1);
+	if (keycode == KEY_ESC)
+		close_window(game);
+	all_coin_collected(game);
+	return (0);
+}
 
-// 	if (argc == 1)
-// 	{
-// 		ft_printf("use it that way---->  ./so_long <map.ber>\n");
-// 		return (0);
-// 	}
-// 	init_game(&game);
-// 	game.filename = argv[1];
-// 	if (new_map(&game))
-// 		return (ft_printf("map not ok\n"), 1);
-// 	if (check_map(&game))
-// 	{
-// 		ft_printf("map not ok :(\n");
-// 		return (free_map(game.map.map, game.map.column), 1);
-// 	}
-// 	if (copy_map(&game) || floodfill(&game, game.player_y, game.player_x))
-// 	{
-// 		free_map(game.map.copy_map, game.map.column);
-// 		free_map(game.map.map, game.map.column);
-// 		init_game(&game);
-// 		return (ft_printf("end impossible\n"), 2);
-// 	}
-// 	free_map(game.map.copy_map, game.map.column);
-// 	game.map.copy_map = NULL;
-// 	ft_printf("\nmap is playable :)\n");
-// 	if (mlx_windows_start(&game))
-// 	{
-// 		free_map(game.map.map, game.map.column);
-// 		return (1);
-// 	}
-// 	return (0);
-// }
+int	mlx_hookloop(t_game *game)
+{
+	mlx_hook(game->mlx.win_ptr, 17, 0, close_window, game);
+	mlx_hook(game->mlx.win_ptr, 2, 1L << 0, update_movement, game);
+	mlx_loop(game->mlx.mlx_ptr);
+	return (0);
+}
--- src/set_window.c
+++ src/set_window.c
@@ -12,69 +12,6 @@
 
 #include "so_long.h"
 
-int	close_window(t_game *game)
-{
-	free_map(game->map.map, game->map.column);
-	free_img_ptr(game);
-	mlx_destroy_window(game->mlx.mlx_ptr, game->mlx.win_ptr);
-	mlx_destroy_display(game->mlx.mlx_ptr);
-	free(game->mlx.mlx_ptr);
-	game->mlx.mlx_ptr = NULL;
-	init_game(game);
-	exit (0);
-	return (0);
-}
-
-void	move_player(t_game *game, int y, int x)
-{
-	if (game->map.map[game->player_y + y][game->player_x + x] == '1')
-	{
-		game->player_mov++;
-		return ;
-	}
-	else
-	{
-		if (game->map.map[game->player_y + y][game->player_x + x] == 'C')
-		{
-			game->map.map[game->player_y + y][game->player_x + x] = '0';
-			game->map.coins_found++;
-		}
-		game->map.map[game->player_y][game->player_x] = '0';
-		sprite_to_put(game, game->player_x, game->player_y);
-		game->map.map[game->player_y + y][game->player_x + x] = 'P';
-		game->player_y += y;
-		game->player_x += x;
-		sprite_to_put(game, game->player_x, game->player_y);
-		game->player_mov++;
-	}
-	ft_printf("position du joueur et de map[%d][%d]\n",
-		game->player_y, game->player_x);
-}
-
-int	update_movement(int keycode, t_game *game)
-{
-	if (keycode == KEY_S)
-		move_player(game, 1, 0);
-	if (keycode == KEY_W)
-		move_player(game, -1, 0);
-	if (keycode == KEY_A)
-		move_player(game, 0, -1);
-	if (keycode == KEY_D)
-		move_player(game, 0, 1);
-	if (keycode == KEY_ESC)
-		close_window(game);
-	all_coin_collected(game);
-	return (0);
-}
-
-int	mlx_hookloop(t_game *game)
-{
-	mlx_hook(game->mlx.win_ptr, 17, 0, close_window, game);
-	mlx_hook(game->mlx.win_ptr, 2, 1L << 0, update_movement, game);
-	mlx_loop(game->mlx.mlx_ptr);
-	return (0);
-}
-
 int	load_sprite(t_game *game)
 {
 	game->sprite.img_player = mlx_xpm_file_to_image(game->mlx.mlx_ptr,
@@ -95,6 +32,42 @@ int	load_sprite(t_game *game)
 	return (0);
 }
 
+void	sprite_to_put(t_game *game, int x, int y)
+{
+	if (game->map.map[y][x] == '0')
+	{
+		mlx_put_image_to_window(game->mlx.mlx_ptr,
+			game->mlx.win_ptr, game->sprite.img_floor, x * 128, y * 128);
+	}
+	if (game->map.map[y][x] == '1')
+	{
+		mlx_put_image_to_window(game->mlx.mlx_ptr,
+			game->mlx.win_ptr, game->sprite.img_wall, x * 128, y * 128);
+	}
+	sprite_to_put_two(game, x, y);
+	return ;
+}
+
+void	sprite_to_put_two(t_game *game, int x, int y)
+{
+	if (game->map.map[y][x] == 'P')
+	{
+		mlx_put_image_to_window(game->mlx.mlx_ptr,
+			game->mlx.win_ptr, game->sprite.img_player, x * 128, y * 128);
+	}
+	if (game->map.map[y][x] == 'E')
+	{
+		mlx_put_image_to_window(game->mlx.mlx_ptr,
+			game->mlx.win_ptr, game->sprite.img_floor, x * 128, y * 128);
+	}
+	if (game->map.map[y][x] == 'C')
+	{
+		mlx_put_image_to_window(game->mlx.mlx_ptr,
+			game->mlx.win_ptr, game->sprite.img_coin, x * 128, y * 128);
+	}
+	return ;
+}
+
 int	put_img(t_game *game)
 {
 	int	x;
@@ -110,36 +83,6 @@ int	put_img(t_game *game)
 			x++;
 		}
 		y++;
-	}
-	return (0);
-}
-
-int	sprite_to_put(t_game *game, int x, int y)
-{
-	if (game->map.map[y][x] == '0')
-	{
-		mlx_put_image_to_window(game->mlx.mlx_ptr,
-			game->mlx.win_ptr, game->sprite.img_floor, x * 128, y * 128);
-	}
-	if (game->map.map[y][x] == '1')
-	{
-		mlx_put_image_to_window(game->mlx.mlx_ptr,
-			game->mlx.win_ptr, game->sprite.img_wall, x * 128, y * 128);
-	}
-	if (game->map.map[y][x] == 'P')
-	{
-		mlx_put_image_to_window(game->mlx.mlx_ptr,
-			game->mlx.win_ptr, game->sprite.img_player, x * 128, y * 128);
-	}
-	if (game->map.map[y][x] == 'E')
-	{
-		mlx_put_image_to_window(game->mlx.mlx_ptr,
-			game->mlx.win_ptr, game->sprite.img_floor, x * 128, y * 128);
-	}
-	if (game->map.map[y][x] == 'C')
-	{
-		mlx_put_image_to_window(game->mlx.mlx_ptr,
-			game->mlx.win_ptr, game->sprite.img_coin, x * 128, y * 128);
 	}
 	return (0);
 }
